a5c9b08b326afcae39ba8345a273004e86c4716b
Kyumin Kim <math4tots@gmail.com>
2016-03-10 11:47:42 -0800

    dijkstrashortreach (rust)

--- lib.rs
+++ lib.rs
@@ -1,20 +1,20 @@
 // ********* Just quick lib *********
 fn readln() -> String {
-    let mut s = String::new();
-    assert!(std::io::stdin().read_line(&mut s).is_ok());
-    s.trim().to_string()
+  let mut s = String::new();
+  assert!(std::io::stdin().read_line(&mut s).is_ok());
+  s.trim().to_string()
 }
 
 fn conv<T>(s: &str) -> T where
-        T: std::str::FromStr,
-        <T as std::str::FromStr>::Err: std::fmt::Debug {
-    s.parse().expect("parse")
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug {
+  s.parse().expect("parse")
 }
 
-fn split<T>(s: String) -> Vec<T> where
-        T: std::str::FromStr,
-        <T as std::str::FromStr>::Err: std::fmt::Debug {
-    s.split_whitespace().map(conv).collect()
+fn split<T>(s: &str) -> Vec<T> where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug {
+  s.split_whitespace().map(conv).collect()
 }
 // ********* end lib *********
 
--- src/dijkstrashortreach.rs
+++ src/dijkstrashortreach.rs
@@ -1,62 +1,74 @@
 // ********* Just quick lib *********
 fn readln() -> String {
-    let mut s = String::new();
-    assert!(std::io::stdin().read_line(&mut s).is_ok());
-    s.trim().to_string()
+  let mut s = String::new();
+  assert!(std::io::stdin().read_line(&mut s).is_ok());
+  s.trim().to_string()
 }
 
 fn conv<T>(s: &str) -> T where
-        T: std::str::FromStr,
-        <T as std::str::FromStr>::Err: std::fmt::Debug {
-    s.parse().expect("parse")
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug {
+  s.parse().expect("parse")
 }
 
-fn split<T>(s: String) -> Vec<T> where
-        T: std::str::FromStr,
-        <T as std::str::FromStr>::Err: std::fmt::Debug {
-    s.split_whitespace().map(conv).collect()
+fn split<T>(s: &str) -> Vec<T> where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug {
+  s.split_whitespace().map(conv).collect()
 }
 // ********* end lib *********
 
 fn main() {
-    let t = conv::<usize>(&readln().trim());
-    for _ in 0..t {
-        let nm: Vec<usize> = split(readln());
-        let n = nm[0];
-        let m = nm[1];
-        let mut e: Vec<Vec<(usize, i32)>> = Vec::new();
-        e.resize(n, Vec::new());
-        for _ in 0..m {
-            let xyv: Vec<i32> = split(readln());
-            let x = xyv[0] as usize - 1;
-            let y = xyv[1] as usize - 1;
-            let v = xyv[2];
-            e[x].push((y, v));
-            e[y].push((x, v));
-        }
-        let s = conv::<usize>(&readln()) - 1;
-        let mut d: Vec<i32> = Vec::new();
-        d.resize(n, -1);
-        let mut q = std::collections::BinaryHeap::new();
-        q.push((s, 0));
-        loop {
-            match q.pop() {
-                Some((node, estimate)) => {
-                    if d[node] == -1 || estimate < d[node] {
-                        d[node] = estimate;
-                        for &(neighbor, cost) in &e[node] {
-                            q.push((neighbor, estimate + cost));
-                        }
-                    }
-                }
-                None => break
-            }
-        }
-        for node in 0..n {
-            if node != s {
-                print!("{} ", d[node]);
-            }
-        }
-        println!("");
+  let t: usize = conv(&readln());
+  for _ in 0..t {
+    // read input
+    let nm = split::<usize>(&readln());
+    let n = nm[0];
+    let m = nm[1];
+    let mut maxdist: i64 = 1;
+    let mut e: Vec<Vec<(usize, i64)>> = Vec::new();
+    e.resize(n, Vec::new());
+    for _ in 0..m {
+      let xyv = split::<i64>(&readln());
+      let x = xyv[0] as usize - 1;
+      let y = xyv[1] as usize - 1;
+      let v = xyv[2];
+      e[x].push((y, v));
+      e[y].push((x, v));
+      maxdist += v;
     }
+    let s = conv::<usize>(&readln()) - 1;
+    // find shortest paths
+    let mut d: Vec<i64> = Vec::new();
+    d.resize(n, maxdist);
+    let mut q: Vec<i64> = Vec::new();
+    q.resize(n, maxdist);
+    q[s] = 0;
+    loop {
+      let mut b = 0;
+      let mut bd = maxdist;
+      for x in 0..n {
+        if d[x] == maxdist && q[x] < bd {
+          b = x;
+          bd = q[x];
+        }
+      }
+      if bd == maxdist {
+        break;
+      }
+      d[b] = bd;
+      for &(ne, cost) in &e[b] {
+        if bd + cost < q[ne] {
+          q[ne] = bd + cost;
+        }
+      }
+    }
+    // print solution
+    for x in 0..n {
+      if x != s {
+        print!("{} ", if d[x] == maxdist { -1 } else { d[x] });
+      }
+    }
+    println!("");
+  }
 }
