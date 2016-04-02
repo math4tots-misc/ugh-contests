dc68ae6113da7d311ea7de901ef3ce1616192ee0
Kyumin Kim <math4tots@gmail.com>
2016-04-02 10:51:01 -0700

    lib/hash.cc

--- lib/hash.cc
+++ lib/hash.cc
@@ -1,5 +1,12 @@
+#include <iostream>
+
 // This is what boost does.
 void hash_combine(std::size_t& seed, const int& v) {
   std::hash<int> hasher;
   seed ^= hasher(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+}
+
+using namespace std;
+int main() {
+  cout << "There is no test for hash.cc" << endl;
 }
